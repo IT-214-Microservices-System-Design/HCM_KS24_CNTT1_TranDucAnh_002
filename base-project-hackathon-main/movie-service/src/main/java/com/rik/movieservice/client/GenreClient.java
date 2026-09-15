bbc6b9b71b457a28e23ec55dd4ef1e9670054066
TranDucAnh3103 <TranDucAnh3103@users.noreply.github.com>
2026-09-15 12:05:10 +0700

    init

--- base-project-hackathon-main/movie-service/src/main/java/com/rik/movieservice/client/GenreClient.java
+++ base-project-hackathon-main/movie-service/src/main/java/com/rik/movieservice/client/GenreClient.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.http.ResponseEntity;
 
-@FeignClient(name = "genre-service")
+@FeignClient(name = "gener-service")
 public interface GenreClient {
     @GetMapping("/api/genres/{id}")
     ResponseEntity<Object> getGenreById(@PathVariable("id") Long id);
